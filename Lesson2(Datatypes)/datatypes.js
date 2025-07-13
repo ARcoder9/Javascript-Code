cb2abde3bf72f8c4e0d6ea6f880264e90f60cdc9
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-13 16:57:26 +0530

    first commit

---
+++ Lesson2(Datatypes)/datatypes.js
@@ -0,0 +1,31 @@
+name1 = "Ayush";
+num = 123;
+bool = true;
+console.log(typeof name1);  // for checking the type of datatypes.
+console.log(typeof num);
+console.log(typeof bool);
+
+// Primitive Data Types: These are immutable, meaning their values cannot be changed after creation.
+// Number: Represents both integer and floating-point numbers.
+
+// String: Represents sequences of characters, enclosed in single quotes, double quotes, or backticks (for template literals).
+
+// Boolean: Represents logical entities with two possible values: true or false.
+
+// Undefined: Represents a variable that has been declared but not yet assigned a value.
+
+// Null: Represents the intentional absence of any object value. It is a primitive value.
+
+// BigInt: Represents whole numbers larger than 2^53 - 1, the maximum number that can be represented by the Number type.
+
+// Symbol: Represents a unique identifier. Symbols are primarily used as object property keys to avoid naming collisions.
+
+
+// Non-Primitive (Reference) Data Type:
+// Object: A complex data type that allows you to store collections of data and more complex entities. Objects are mutable, meaning their properties can be changed after creation. This category includes:
+
+// Arrays: Ordered collections of values, accessed by index.
+
+// Functions: Callable objects that encapsulate reusable blocks of code.
+
+// Dates, Regular Expressions, Maps, Sets, etc.: Other built-in object types.
---
+++ Lesson3(Variables)/Variables.js
@@ -0,0 +1,3 @@
+var myname = "Ayush";    // Keyword Name = Value;
+var yourname = prompt("what is your name?");   // it is used to take the input from the user
+alert("my name is " + myname + " and your name is " + yourname);  // it shows the alertg
