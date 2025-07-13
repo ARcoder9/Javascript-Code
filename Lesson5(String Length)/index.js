a0bc42f300691c681f9091a49d4f2a5bf8dcb8de
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-13 20:17:43 +0530

    string length and slicing

---
+++ Lesson5(String Length)/index.js
@@ -0,0 +1,10 @@
+var tweet = prompt("write your tweet here!!!")
+var tweet_count = tweet.length;
+
+alert("you have written " + tweet_count + " Characters you have " + (140 - tweet_count) + " Characters remaining");
+
+
+
+var name = "Ayush";
+var ans = name.slice(0,1)  // it helps to slice the character from the string 
+console.log(ans);     // if you want to know how many character you will get formula is Upper_bound - Lower_bound
