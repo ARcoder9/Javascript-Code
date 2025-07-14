c8cdae19f8e0891a2e2af6f83e750fa409c263c0
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-14 23:43:31 +0530

    Arithmetic operators

---
+++ Lesson6(Arithmetic)/index.js
@@ -0,0 +1,11 @@
+var divide = 6/4;
+var sub = 6-4;
+var add = 6+4;
+var mod = 6%4;
+var multi = 6*4;
+
+console.log(divide);
+console.log(sub);
+console.log(add);
+console.log(mod);
+console.log(multi);
