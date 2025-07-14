d6f3dcfc391b30cdb4fd841ee1a4264108fa61a6
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-15 00:00:45 +0530

    challenge question

--- Lesson6(Arithmetic)/index.js
+++ Lesson6(Arithmetic)/index.js
@@ -1,11 +1,20 @@
-var divide = 6/4;
-var sub = 6-4;
-var add = 6+4;
-var mod = 6%4;
-var multi = 6*4;
+var divide = 6/4;  // divide operator
+var sub = 6-4;    // substract operator
+var add = 6+4;    // additon operator
+var mod = 6%4;    // modulus operator
+var multi = 6*4;  // multiplication operator
 
 console.log(divide);
 console.log(sub);
 console.log(add);
 console.log(mod);
 console.log(multi);
+
+
+// ##Challenge Question :- compare dog age with human age
+
+// formula ==> human age = (dog age - 2) * 4 + 21
+
+var dog_age = prompt("what is your dog_age?");
+var human_age = (dog_age - 2) * 4 + 21;
+alert("your dog age as compare to human is " + human_age);
