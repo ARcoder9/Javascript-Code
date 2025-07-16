d263ec98c111b6637d517beab97e9e10b87c76d0
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-16 22:55:05 +0530

    functions

---
+++ Lesson7(Functions)/index.js
@@ -0,0 +1,8 @@
+function getMilk(){      // creating the function
+       console.log("move right");
+       console.log("move left");
+       console.log("move straight");
+       console.log("move back");
+};
+
+getMilk();    //calling the function
