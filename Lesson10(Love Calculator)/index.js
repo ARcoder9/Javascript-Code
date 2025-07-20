178505603b68a34a0a551ab8bf9b8cf8f80a7d66
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-20 18:01:55 +0530

    Love Calculator

---
+++ Lesson10(Love Calculator)/index.js
@@ -0,0 +1,8 @@
+// Building a Love Calculator
+
+prompt("What is your name ?");
+prompt("What is your lover's name ?");
+
+var n = Math.random() * 100; // it helps to create random number between 0 to 99
+var output = Math.floor(n) + 1; // math.floor for round off and adding one it make range between 1 to 100
+alert("your love score is " + output + "%");
