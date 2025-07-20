5f8647e732d8ea704467bf5deaad96d2b3e7adca
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-21 01:58:31 +0530

    for loop

---
+++ Lesson20 (For Loop)/index.js
@@ -0,0 +1,15 @@
+var i;
+for(i=0; i<5; i++){     // for loop in javaScript
+    console.log(i);
+}
+//          [1]    [2]    [4]
+//     for(start;  end; change){
+//      statement [3]     
+//     }
+
+// first pointer goes on start and then check the end condition then runs the statement and at last increment the value
+
+
+// use of loops
+// while loop is used when something is true all run the program untill the particular STATE.
+// For loop is used when we have to ITERATE something to many no. of time .
