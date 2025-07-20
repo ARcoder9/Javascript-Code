d8bac253fbcb27caeaaf0dd0126133d6b5e25c0f
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-20 17:03:00 +0530

    comparators

---
+++ Lesson12(Comparators)/index.js
@@ -0,0 +1,12 @@
+var a =5;
+var b=7;
+var c = "7";
+
+
+console.log(b==c);    // is equal to
+console.log(b===c);   // is equal to and also compares the data-type
+console.log(a!==b);   // is not equal to
+console.log(a>b);     // is greater than
+console.log(a<b);     // is lesser than
+console.log(a>=b);    // is greater or eqaual to
+console.log(a<=b);    // is lesser or equal to
