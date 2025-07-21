0ffb530aae17d2b9386ddb1f50f568ce1b2917af
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-07-22 00:53:17 +0530

    modify

--- Lesson23(Challenge --> Dice Game)/index.js
+++ Lesson23(Challenge --> Dice Game)/index.js
@@ -1,24 +1,8 @@
-// first basic approach of doing this
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+ //  !-> first basic approach of doing this
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+//var dice_1 = Math.floor(Math.random() * 6) + 1;
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 wins";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 wins";
-} else {
-  document.querySelector("h1").innerHTML = "Match Tie";
-}
-
-
-// var dice_1 = Math.floor(Math.random() * 6) + 1;
-
-// var dice_2 = Math.floor(Math.random() * 6) + 1;
+//var dice_2 = Math.floor(Math.random() * 6) + 1;
 
 
 // if(dice_1 == 1){
@@ -75,6 +59,21 @@ if (randomNumber1 > randomNumber2) {
 
 
 
-// How good programmer approach this question
+//  !->  How good programmer approach this question
 
 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+  document.querySelector("h1").innerHTML = "Match Tie";
+}
