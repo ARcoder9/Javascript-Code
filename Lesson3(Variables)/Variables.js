f3ea8e5c6af4aac0bb537fdb8fa5daaf95ec5ba7
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2025-08-16 20:38:06 +0530

    variables

--- Lesson3(Variables)/Variables.js
+++ Lesson3(Variables)/Variables.js
@@ -1,3 +1,22 @@
-var myname = "Ayush";    // Keyword Name = Value;
-var yourname = prompt("what is your name?");   // it is used to take the input from the user
-alert("my name is " + myname + " and your name is " + yourname);  // it shows the alertg
+// var myname = "Ayush";    // Keyword Name = Value;
+// var yourname = prompt("what is your name?");   // it is used to take the input from the user
+// alert("my name is " + myname + " and your name is " + yourname);  // it shows the alertg
+
+
+// 
+
+var a = 5;
+var b = 6;
+var c = "harry";
+let d = 9;
+{
+    let d = 10;     // it is block scope variable in modern approach use let 
+    console.log(d);
+}
+console.log(d);
+
+const a1 = 6;
+a1 = a1 + 1;  // Not allowed because a1 is constant.
+
+console.log(a + b + 8);
+console.log(typeof(a), typeof(b), typeof(c));
